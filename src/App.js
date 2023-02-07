248b30b8ecf9648b5f8d2c45821e16b3eca8589c
AzisKu <118185314+AzisKu@users.noreply.github.com>
2023-02-08 00:46:17 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,42 +27,6 @@ export default function (props){
     )
 }
 
-// Пример с использованием State
-// class Button extends React.component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             count: 0,
-//         };
-//     }
-//
-//     updateCount() {
-//         this.setState((prevState, props) => {
-//             return{count: prevState.count + 1}
-//         });
-//     }
-//
-//     render() {
-//         return (<button
-//             onClick={() => this.updateCount()}
-//         >
-//             Clicked{this.state.count} times
-//         </button>);
-//
-//
-//     }
-//
-// }
-
-
-// props Пример
-// class Welcome extends React.Component {
-//     render() {
-//         return <h1>Hello {this.props.name}</h1>;
-//     }
-// }
-//
-// const element = <Welcome name="Sara" />;
 
 // код из перовго урока (секундомер)
 // class Timer extends React.Component {
@@ -95,47 +59,85 @@ export default function (props){
 
 // export default Timer;
 
-import React from "react";
+// import React from "react";
 
-class EssayForm extends React.Component{
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: "Please,write essay about your favorite music,my favorite music mostly is rock music,also i like subgenres like:Pop punk,alternative rock,electro rock-Pop"};
+// class EssayForm extends React.Component{
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             value: "Please,write essay about your favorite music,my favorite music mostly is rock music,also i like subgenres like:Pop punk,alternative rock,electro rock-Pop"};
             
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-    handleSubmit(event){
-        alert("Отправленное имя" + this.state.value);
-        event.preventDefault()
-    }
-    render(){
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Эссе:
-                    <textarea value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Отправить"/>
-            </form>
-        );
-    }
-}
+//     handleChange(event) {
+//         this.setState({value: event.target.value});
+//     }
+//     handleSubmit(event){
+//         alert("Отправленное имя" + this.state.value);
+//         event.preventDefault()
+//     }
+//     render(){
+//         return(
+//             <form onSubmit={this.handleSubmit}>
+//                 <label>
+//                     Эссе:
+//                     <textarea value={this.state.value} onChange={this.handleChange} />
+//                 </label>
+//                 <input type="submit" value="Отправить"/>
+//             </form>
+//         );
+//     }
+// }
+
 
 export default EssayForm;
 
-// {/*// choose a favorite fruit:*/}
-// {/*<select value={this.state.value} onChange={this.handleChange}>*/}
-// {/*    <option value="wildberries">ЛесныеЯгоды</option>*/}
-// {/*    <option value="kiwi">Киви</option>*/}
-// {/*    <option value="strawberry">Клубника</option>*/}
-// {/*    <option value="raspberry">Малина</option>*/}
-// {/*</select>*/}n
+// пример №1 Жизненный Цикл Компонента
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div>{date.toLocaleTimeString()}</div>
+    );
+  }
+}
+// пример №2 с методом componentWillunmout
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.setState({ date: new Date() }), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    const { date } = this.state;
+    return (
+      <div>{date.toLocaleTimeString()}</div>
+    );
+  }
+}
+
+
+
 
 
 
